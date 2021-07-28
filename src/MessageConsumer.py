c8903ee14d177f0586e5f77ba1b79f517f16852f
davidhieber <david.hieber@uni-tuebingen.de>
2021-07-28 16:18:38 +0200

    fixed the example json and startet email credantiols(load from file later on has to be replacet with os getenv, addtet logger analuges to train router , addet the first tow cases for massege procceser

--- src/MessageConsumer.py
+++ src/MessageConsumer.py
@@ -1,11 +1,20 @@
-from typing import Union
+import json
 
 from train_lib.clients import Consumer
+from train_lib.clients.rabbitmq import LOG_FORMAT
+from MessageDistributor import MessageDistributor
+import logging
+
+LOGGER = logging.getLogger(__name__)
 
 
-class MassageConsummer(Consumer):
-    def __init__(self, amqp_url: str):
-        super().__init__(amqp_url)
+class MassageConsumer(Consumer):
+    def __init__(self, amqp_url: str, queue: str = "", routing_key: str = None, ui_user: str = None,
+                 ui_token: str = None):
+        super().__init__(amqp_url, queue, routing_key=routing_key)
+        self.ui_token = ui_token
+        self.ui_user = ui_user
+        self.md = MessageDistributor()
 
     def run(self):
         super().run()
@@ -13,16 +22,37 @@ class MassageConsummer(Consumer):
     def on_message(self, _unused_channel, basic_deliver, properties, body):
         pass
 
-    def process_message(self, msg: Union[dict, str]):
-        pass
+    def process_message(self, msg):
+        if msg["type"] == "proposalOperationRequired":
+            self.md.process_proposal_operation_required(msg["data"])
+        elif msg["type"] == "trainStarted":
+            self.md.process_train_started(msg["data"])
+        else:
+            LOGGER.info(f"Invalid event {msg['type']}")
 
 
 def main():
     # TODO login parameters only for developing later in docker -compose as env varibals
-    vault_token = " the token "
-    AMPQ_URL = "https://pht.tada5hi.net/api "
-    massage_consumer = MassageConsummer(AMPQ_URL)
+    # only for testign later with os.getenv("smtp_user") etc , ,..
+    login_credentials_file = open("../static_setup.json", "r")
+    credentials_json = json.load(login_credentials_file)
 
+    AMPQ_URL = credentials_json["AMPQ_URL"]
+    ui_user = credentials_json["ui_user"]
+    ui_token = credentials_json["ui_token"]
+
+    logging.basicConfig(level=logging.INFO, format=LOG_FORMAT)
+    massage_consumer = MassageConsumer(AMPQ_URL, ui_user=ui_user, ui_token=ui_token, routing_key="en.event")
+
+    # static test
+    sample_message_file = open("../example_message_proposal.json", "r")
+    sample_message_json = json.load(sample_message_file)
+    pprint_json(sample_message_json)
+    massage_consumer.process_message(sample_message_json)
+
+
+def pprint_json(data):
+    print(json.dumps(data, indent=2, sort_keys=True))
 
 
 if __name__ == '__main__':
--- src/MessageDistributor.py
+++ src/MessageDistributor.py
@@ -1,24 +1,48 @@
-class MessageDistributor():
+import json
+import smtplib, ssl
+
+
+class MessageDistributor:
     def __init__(self):
-        users = self.request_users()
+        # users = self.request_users()
+        # TODO send emails to spesific users , for know send only to me for testing
+        # only for testign later with os.getenv("smtp_user") etc , ,..
+        login_credentials_file = open("../static_setup.json", "r")
+        credentials_json = json.load(login_credentials_file)
+        self.smtp_user = credentials_json["smtp_user"]
+        self.smtp_password = credentials_json["smtp_password"]
+        self.smtp_mail_from = "pht@medizin.uni-tuebingen.de"
+        self.port = 587
+        self.smtp_host = "smtpserv.uni-tuebingen.de"
+
+        self.mail_target = "david.hieber@uni-tuebingen.de"
+
+        self._setup_smtp()
+
+    def process_proposal_operation_required(self, data):
+        pprint_json(data)
+        self._send_email_to()
+
+    def process_train_started(self, data):
         pass
 
-    def send_email_to(self, name, ):
-        pass
-
-    def get_admins_for_realm(self, realm):
+    def _send_email_to(self, name, body):
         pass
 
     def request_users(self):
         pass
 
-    def setup_smpt(self):
+    def _setup_smtp(self):
         pass
 
-    class User():
+    class User:
         def __init__(self):
             self.name = ""
             self.email = ""
             self.realm = ""
             self.send_mail_on_new_train = False
             self.send_mail_on_new_redy_to_build = False
+
+
+def pprint_json(data):
+    print(json.dumps(data, indent=2, sort_keys=True))
