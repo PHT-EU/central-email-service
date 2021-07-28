60b58c37d70d235f9d57d19d476aaabd2eef5ba0
davidhieber <david.hieber@uni-tuebingen.de>
2021-07-28 11:36:41 +0200

    add requirements.txt and layout for the massage consummer

---
+++ src/MessageConsumer.py
@@ -0,0 +1,25 @@
+from typing import Union
+
+from train_lib.clients import Consumer
+
+
+class MassageConsummer(Consumer):
+    def __init__(self, amqp_url: str):
+        super().__init__(amqp_url)
+
+    def on_message(self, _unused_channel, basic_deliver, properties, body):
+        pass
+
+    def process_message(self, msg: Union[dict, str]):
+        pass
+
+
+def main():
+    # TODO login parameters only for developing later in docker -compose as env varibals
+    vault_token = " the token "
+    AMPQ_URL = "https://pht.tada5hi.net/api "
+    massage_consumer = MassageConsummer(AMPQ_URL)
+
+
+if __name__ == '__main__':
+    main()
